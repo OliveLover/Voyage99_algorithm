f48c35d4199da3628ce2edd77a98becc4a1cf479
OliveLover <118647313+OliveLover@users.noreply.github.com>
2023-03-08 14:33:00 +0900

    Add files via upload

---
+++ 1day/3.2869/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int v = scanner.nextInt();
+
+        System.out.println((int)Math.ceil((v-b)/(a-b)));
+    }
+}
